56e9b84789ff5ba9e2bc64281da365fc4ff5663b
Matteo Repetto <matteo.repetto@ge.imati.cnr.it>
2021-02-01 18:47:36 +0100

    Added more fileds inthe info collected by BPF.

--- common.h
+++ common.h
@@ -24,8 +24,54 @@ struct flow_id {
  *	TODO: add support for more statistics.
  */
 struct flow_info {
-	__u32		pkts;
-	__u32		bytes;
+	/* Generic flow information (for all protocols) */
+	__u64	first_seen;		/* Epoch of the first packet of this flow (us). */
+	__u64	last_seen;	  	/* Epoch of the last packet seen so far (us). */
+	__u32	pkts;		    	/* Cumulative number of packets. */
+	__u32	bytes;		    	/* Cumulative number of bytes. */
+	__u32	jitter;			/* Cumulative delays between packets. */
+	__u16	min_pkt_len;	 	/* Smallest IP packet seen in the flow. */
+	__u16	max_pkt_len; 		/* Biggest IP packet seen in the flow. */
+	__u16	pkt_size_hist[6];	/* [0]: pkts up to 128 bytes;
+					 * [1]: pkts from 128 to 256 bytes;
+					 * [2]: pkts from 256 to 512 bytes;
+					 * [3]: pkts from 512 to 1024 bytes;
+					 * [4]: pkts from 1024 to 1514 bytes;
+					 * [5]: pkts over 1514 bytes.
+					 */
+	__u16	pkt_ttl_hist[10];	/* [0]: pkts with TTL=1;
+					 * [1]: pkts with TTL>1 and TTL<=5;
+					 * [2]: packets with TTL > 32 and <= 64;
+					 * [3]: packets with TTL > 32 and <= 64;
+					 * [4]: packets with TTL > 64 and <= 96;
+					 * [5]: packets with TTL > 96 and <= 128;
+					 * [6]: packets with TTL > 128 and <= 160;
+					 * [7]: packets with TTL > 160 and <= 192;
+					 * [8]: packets with TTL > 192 and <= 224;
+					 * [9]: packets with TTL > 224 and <= 255.
+					 */
+
+	/* IP-related filds and measurements. */
+	__u8 	version;		/* Version (4/6) */
+	__u8	tos;		   	/* TOS/DSCP (IPv4) or Traffic Class (IPv6). */	
+	__u32	fl;			/* Flow label (IPv6 only). */
+	__u8	min_ttl;		/* Min TTL (IPv4) or Hop Limit (IPv6). */
+	__u8	max_ttl;		/* Max TTL (IPv4) or Hop Limit (IPv6). */
+
+	/* TCP-related fields. */
+	__u8		cumulative_flags; /* Cumulative TCP flags seen in all packets so far. */
+	__u16	retr_pkts;		/* Total number of retrasmitted packets. */
+	__u32	retr_bytes;		/* Total number of retransmitted bytes. */
+	__u16	ooo_pkts;		/* Total number of out-of-order packets. */
+	__u32	ooo_bytes;		/* Total number of out-of-order bytes. */
+	__u16	min_win_bytes;		/* Min TCP Windows. */
+	__u16	max_win_bytes;		/* Max TCP Windows. */
+	__u16	mss;			/* TCP Max Segment Size. */
+	__u8	wndw_scale;		/* TCP Window Scale. */
+	__u16	min_wndw;		/* Min TCP Window. */
+	__u16	max_wndw;		/* Max TCP window. */
+
+	/* Other NetFlow or IPFIX fields are L7- or mgmt specifics and are not collected through packets. */
 };
 
 /* Exit return codes */
